dd1a32eefb76fd37ee6834fc92945ff07a129185
akshshukla2 <akshitashukla691@gmail.com>
2025-05-18 00:37:24 +0530

    js commit 1

---
+++ web_development/lesson_19/activity.js
@@ -0,0 +1,28 @@
+// all in one 
+
+// SAYNTAX GUIDE AND COMMENTING
+console.log("Akshita Shukla is printing.")
+
+//variables
+var a = 30
+var b = 20
+var c = "Randomly typing this."
+
+//statements
+
+if (a > b)
+    console.log("a is greater than b")
+else if (b > a)
+    console.log("b is greater than a")
+else
+    console.log("both are equal")
+
+// operators
+var d = a + b
+console.log(d)
+var e = 30
+var f=2
+var g=4;
+ e /= f 
+console.log(e)
+
