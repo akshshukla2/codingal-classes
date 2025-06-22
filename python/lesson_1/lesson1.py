6823acb28a8ba69027cf355ad5f1d41f44479ac9
akshshukla2 <akshitashukla691@gmail.com>
2025-06-22 22:21:29 +0530

    python 1

---
+++ python/lesson_1/lesson1.py
@@ -0,0 +1,16 @@
+# This is a comment 
+print("PRINTING THIS SENTENCE.") #printing 
+
+#Making a function
+def func1():
+    print("PRINTING UNDER FUNCTION 1")
+    print("PRINTING UNDER FUNCTION 1")
+
+
+func1() #calling a function
+
+a = "variable 1" #making a variable
+a = 90
+a = "variable 2"
+
+print(a) #printing a variable 
