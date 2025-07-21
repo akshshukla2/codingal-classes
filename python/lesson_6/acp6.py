da07418d3eb1012855c3dc01e9f7c01e4afabcec
akshshukla2 <akshitashukla691@gmail.com>
2025-07-21 09:07:49 +0530

    Update

--- python/lesson_6/acp6.py
+++ python/lesson_6/acp6.py
@@ -1,7 +1,7 @@
 import turtle
 
 turtle.Screen().bgcolor("aqua")
-
+# for teacher : i have also done equilateral triangle but that is in "acp6pt2.py" folder because i dont know how to do it in one.
 sc = turtle.Screen()
 
 sc.setup(500, 500)
