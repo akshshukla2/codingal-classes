87e940e010dee917101245d7aa640edd1681e050
akshshukla2 <akshitashukla691@gmail.com>
2025-08-15 22:14:25 +0530

    commit

---
+++ python/lesson_9/lesson9.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def intro(self):
+        print(f"Hey! My name is {self.name} and I am {self.age} years old.")
+
+    def greet(self, name2):
+        print(f"Hello {self.name}! I am {name2}")
+    
+
+a1= Animal('Akshita', 19)
+a2= Animal('Pranav', 19)
+print(a1.name)
+print(a1.age)
+a1.intro()
+a2.intro()
+
+a1.greet('Pranav')
+a2.greet('Akshita')
+      
